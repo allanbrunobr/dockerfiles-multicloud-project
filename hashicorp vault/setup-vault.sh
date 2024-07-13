e1b8a576ab1196507a5429fd7521d53cf9dce9ec
allanbrunobr <allanbruno@gmail.com>
2024-07-13 18:35:13 -0300

    new image

--- hashicorp vault/setup-vault.sh
+++ hashicorp vault/setup-vault.sh
@@ -13,13 +13,9 @@ vault login $VAULT_TOKEN
 
 vault policy write providers-policy /vault/config/providers-policy.hcl
 
-#vault kv put secret/gcp google_api_key_places=AIzaSyBMnDHDoSUU1t1DqEXWiF7FheEeMKHNm5A
-#vault kv put secret/aws access_key_id=AKIAVRUVTFYROQ5MQ3CG secret_access_key=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
+vault kv put secret/gcp google_api_key_places=AIzaSyBMnDHDoSUU1t1DqEXWiF7FheEeMKHNm5A
 
 #vault kv put secret/azure client_id=22ba032b-0cd7-4920-9677-1767839d8420 client_secret=2_U8Q~1CJNJ7XAzjRUXUbLQiWCrtVDDf2faHRaZ~ tenant_id=9f091ec7-cb56-407d-8aff-3af3a6f6878e
-
-#vault kv put secret/gcp @/vault/config/gcp_secret.json
-
 
 
 #AWS
@@ -46,19 +42,11 @@ vault write aws/roles/my-role \
 
 
 # GCP
-#vault secrets enable gcpkms
 vault secrets enable gcp
 
 vault write gcp/config credentials=@gcp_secret.json
-
-# vault write gcp/static-account/my-token-account \
-#     service_account_email="sa-288@app-springboot-project.iam.gserviceaccount.com" \
-#     secret_type="access_token" \
-#     token_scopes="https://www.googleapis.com/auth/cloud-platform" \
-#     bindings=@gcp_access_policy.hcl
 
 vault write gcp/roleset/my-key-roleset \
     project="app-springboot-project" \
     secret_type="service_account_key"  \
     bindings=@gcp_access_policy.hcl
-# vault read gcp/static-account/my-token-account/token
